647e5c5df7009385ac0d45ebcc3bdf22a60e474e
pamelajoy <pbuschbacher@gmail.com>
2016-11-17 19:11:46 -0600

    thru lesson 4 added

---
+++ unit6/unit-6-lesson-4/js/main.js
@@ -0,0 +1,19 @@
+$('button').on('click', function () {
+
+	// Add your code here!
+
+	// Step 1: Add the party class to the .square
+  $('.square').addClass('party');
+
+
+	// Step 2: Use the .attr() method to update the src of the image to 'images/cat.jpeg'
+$('img').attr('src', 'images/cat.jpeg');
+
+	// Step 3: Change the HTML for the h1 to "Having fun!" using the .html() method.
+
+$('h1').html("Having fun!");
+	// Step 4: Prepend the following paragraph to the body element:
+	//    '<p>Done and Done!</p>'
+$('body').prepend('<p>Done and Done!</p>');
+
+});
